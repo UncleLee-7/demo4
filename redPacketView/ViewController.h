f8c12d7d1354df5ff45d0b04a7cab940f4af09a8
paradise_ <paradise_yjgx@192.168.2.121>
2017-09-18 17:55:35 +0800

    Initial Commit

---
+++ redPacketView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  redPacketView
+//
+//  Created by paradise_ on 2017/9/18.
+//  Copyright © 2017年 YJGX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ redPacketView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  redPacketView
+//
+//  Created by paradise_ on 2017/9/18.
+//  Copyright © 2017年 YJGX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
