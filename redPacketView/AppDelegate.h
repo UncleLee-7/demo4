581f0062b66a42fa7ecc4364ca9737e3af032952
paradise_ <paradise_yjgx@192.168.2.121>
2017-09-18 17:55:35 +0800

    Initial Commit

---
+++ redPacketView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  redPacketView
+//
+//  Created by paradise_ on 2017/9/18.
+//  Copyright © 2017年 YJGX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ redPacketView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  redPacketView
+//
+//  Created by paradise_ on 2017/9/18.
+//  Copyright © 2017年 YJGX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
